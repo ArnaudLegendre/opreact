33ee3ba063e1bd6fa868f7d6d50e7725f83b1faa
Florian <Florian@iMac-de-Florian.local>
2021-11-04 11:19:54 +0100

    fix failed to create sync storage

--- Store/configureStore.js
+++ Store/configureStore.js
@@ -1,11 +1,11 @@
 import { createStore } from 'redux';
 import toggleFavorite from './Reducers/favoriteReducer'
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const rootPersistConfig = {
     key: 'root',
-    storage: storage
+    storage: AsyncStorage
 }
 const persistedReducer = persistReducer(rootPersistConfig, toggleFavorite)
 export default createStore(persistedReducer)
